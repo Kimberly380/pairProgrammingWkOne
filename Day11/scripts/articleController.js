629158fa6d59cf80b00d7efbc42915b295b9fc88
Johnny Luangphasy <johnny@jluangphasy.com>
2016-04-25 14:35:38 -0700

    Finish admin page

--- Day11/scripts/articleController.js
+++ Day11/scripts/articleController.js
@@ -7,12 +7,19 @@
 
   // DONE: Complete this function below that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
-  
+
   Article.fetchAll(articleView.initIndexPage);
-  
-  
+
+
   // TODO: Also be sure to hide all the main section elements, and reveal the #articles section:
   articlesController.index = function() {
+    $('title').text('Blog');
+
+    var $mainNav = $('.main-nav');
+    $mainNav.find('li').hide();
+    $mainNav.find('li:lt(4)').show();
+    $mainNav.find('li:eq(0) > a').text('Home');
+
     $('main > section').hide();
     $('section#articles').show();
   };
@@ -23,6 +30,3 @@
 
 
 //need to call this function!
-
-
-  
