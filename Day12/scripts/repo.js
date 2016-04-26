3b6116a787173868a0f57016f6b946e42d7418b7
Kim Feinberg <kimberly380@gmail.com>
2016-04-26 12:05:17 -0700

    added git ignore file.  made notes from lecture

--- Day12/scripts/repo.js
+++ Day12/scripts/repo.js
@@ -6,7 +6,13 @@
   repos.requestRepos = function(callback) {
     // TODO: How would you like to fetch your repos? Don't forget to call the callback.
     // Hint: What did you learn on Day 6? Use the method that lets you send a HEAD
-  };
+
+//getJSON('api.github.com/....', function(data){
+    //process data (extract just the repo names) (header arguements...put token here???)
+    //clall the DOM rendering callback function 
+    //callback();
+ //}))  
+};
 
   // DONE: Model method that filters the full collection for repos with a particular attribute.
   // You could use this to filter all repos that have a non-zero `forks_count`, `stargazers_count`, or `watchers_count`.
