0461d47b0819f751c1e6dc63937f3e864594daac
Kim Feinberg <kimberly380@gmail.com>
2016-04-26 14:17:37 -0700

    working on pulling data from github api

--- Day12/scripts/githubToken.js
+++ Day12/scripts/githubToken.js
@@ -1,3 +1,3 @@
 // DONE: Your token should look something like this:
 //         11800bfd92cd60fb1aa6b020870384bdf25cc471
-var githubToken = '2c597de1a6a254d1a99c0b677fb09adbeaeb81cc'; // <-- TODO
+var githubToken = '2c597de1a6a254d1a97'; // <-- TODO
--- Day12/scripts/repo.js
+++ Day12/scripts/repo.js
@@ -1,25 +1,44 @@
-(function(module) {
   var repos = {};
   repos.all = [];
 
-  repos.requestRepos = function(callback) {
-    // TODO: How would you like to fetch your repos? Don't forget to call the callback.
-    // Hint: What did you learn on Day 6? Use the method that lets you send a HEAD
+ repos =$.ajax({
+   dataType:'json',
+   url: 'https://api.github.com/user',
+   data: 'Authorization token '+githubToken,
+   success: console.log('success!')   
+ })
+ 
+  // $.getJSON('https://api.github.com/users/' + githubToken+'/repos?callback=?');
 
-//getJSON('api.github.com/....', function(data){
-    //process data (extract just the repo names) (header arguements...put token here???)
-    //clall the DOM rendering callback function 
-    callback();
- //}))  
-};
+
+// (function(module) {
+
+//   repos.requestRepos = function(callback) {
+//      $.getJSON('/api.github.com/user/' + githubToken, function(data){
+//     //process data (extract just the repo names) (header arguements...put token here???)
+ 
+//     // }).map(function(key){
+//     //   return repos.all.push(repos.name
+//     //   repos.description)  
+      
+//     // });
+//       callback();
+//      });
+//   } 
 
   // DONE: Model method that filters the full collection for repos with a particular attribute.
   // You could use this to filter all repos that have a non-zero `forks_count`, `stargazers_count`, or `watchers_count`.
-  repos.with = function(attr) {
-    return repos.all.filter(function(repo) {
-      return repo[attr];
-    });
-  };
+  // repos.with = function(attr) {
+  //   return repos.all.filter(function(repo) {
+  //     return repo[attr];
+  //   });
+  // };
 
-  module.repos = repos;
-})(window);
+//   module.repos = repos;
+// })(window);
+
+
+
+
+   // TODO: How would you like to fetch your repos? Don't forget to call the callback.
+    // Hint: What did you learn on Day 6? Use the method that lets you send a HEAD
